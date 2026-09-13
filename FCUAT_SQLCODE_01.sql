7b3c720f037afd605a526ee1b69e34b6ad61f77e
Prashant-git-coder <pnarale1072@gmail.com>
2026-09-13 16:18:29 +0530

    Changes commited 03 times

--- FCUAT_SQLCODE_01.sql
+++ FCUAT_SQLCODE_01.sql
@@ -41,5 +41,5 @@ WHERE dept_rank = 2 ORDER BY department_id DESC;4
 
 
 
-select * from table  DUPLICATE :
-=======================================================================PENDING CODE   
+--CODE IS REDAY FOR NEXT 
+=========================--CODE IS REDAY FOR NEXT 
