6492a72a09fad54e0a56a5557877941b32a32016
Prashant-git-coder <pnarale1072@gmail.com>
2026-09-13 18:04:04 +0530

    Git Restore Command

---
+++ FCUAT_SQLCODE_03.sql
@@ -0,0 +1,39 @@
+SQL*Plus: Release 11.2.0.2.0 Production on Mon Jan 30 14:50:28 2023
+
+Copyright (c) 1982, 2014, Oracle.  All rights reserved.
+
+SQL> conn aakash/aakash;
+Connected.
+SQL> select * from tab;
+
+TNAME                          TABTYPE  CLUSTERID
+------------------------------ ------- ----------
+CUSTOMER                       TABLE
+DEPT                           TABLE
+EMP                            TABLE
+EMP7                           TABLE
+EMPLOYEE                       TABLE
+EMPLOYEE_INCENTIVE             VIEW
+EMP_BACKUP                     TABLE
+FIND_AGE                       TABLE
+GM                             TABLE
+INCENTIVES                     TABLE
+JOB                            TABLE
+
+TNAME                          TABTYPE  CLUSTERID
+------------------------------ ------- ----------
+M_M2                           TABLE
+PR                             TABLE
+SALGRANDE                      TABLE
+SQ_COUNTRIES                   TABLE
+SQ_JOB_HISTORY                 TABLE
+SQ_REGIONS                     TABLE
+TGT_COUNTRIES                  TABLE
+TGT_LAPTOP                     TABLE
+TGT_M_AA                       TABLE
+TGT_M_MAPPING1                 TABLE
+WWE                            TABLE
+
+
+----git Restore comands --- FIRST 1 RESTORE WITHOUT COMMITE 	
+                        ---	SECOND 2 RESTORE WITHOUT COMMITE
