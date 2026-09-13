50725cc2a5afd78d68e1a96f12da9ac871e85503
Prashant-git-coder <pnarale1072@gmail.com>
2026-09-13 20:02:29 +0530

    GIT RESET COMMAND

---
+++ FCUAT_SQLCODE_04.sql
@@ -0,0 +1,15 @@
+SQL*Plus: Release 11.2.0.2.0 Production on Mon Jan 30 14:50:28 2023
+
+Copyright (c) 1982, 2014, Oracle.  All rights reserved.
+
+SQL> conn aakash/aakash;
+Connected.
+SQL> select * from tab;
+
+************** -- Git Reset Command Explained
+
+--	WD01 -->CODE --> COMMIT-->STAGING -->LOCAL REP-->REMOTE HOST :GITHUB ---> COMMITED CODE ROLLBACK FROM REMOTE 
+
+---- ONE CODE MADE 
+CREATE INDEX idx_sale_date_partition ON sales_data (sale_date) LOCAL;
+
