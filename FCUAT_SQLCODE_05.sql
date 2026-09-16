09306d5e219a2b60d222bea02ec44dedd87e8dcc
Prashant-git-coder <pnarale1072@gmail.com>
2026-09-16 20:42:17 +0530

    Git Diff Command

--- FCUAT_SQLCODE_05.sql
+++ FCUAT_SQLCODE_05.sql
@@ -2,7 +2,6 @@ select  employee_id,department_id,salary
 from employees 
 where salary in 
     (
-----	PRASHANT NARALE 
     select max(salary) from employees where salary < 
     (select max(salary) from employees )
     );
